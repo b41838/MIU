cac891823bacaff73be646b8fe92a5ad9627d86e
Kyler Schroeder <kylerschroeder@fullsail.edu>
2013-05-26 23:59:13 -0600

    Midnight Commit
    Fully functioning CRUD, which a few quirks.  When you edit an option,
    it doesn't show the checkbox as checked or the select menu item, but
    when you save, it saves correctly.  Also, does not change button text
    to Update.

--- week4/Gold/js/main.js
+++ week4/Gold/js/main.js
@@ -24,6 +24,14 @@ $('#addItem').on('pageinit', function(){
 		var theElement = document.getElementById(x);
 		return theElement;
 	}
+
+	function isRequired() {
+		if(go('req').checked) {
+			requiredPill = go('req').value;
+		} else {
+			requiredPill = "No"
+		}
+	}
 	
 	function toggleControls(n) {
 		switch(n) {
@@ -45,15 +53,83 @@ $('#addItem').on('pageinit', function(){
 		}
 	}
 
+	// get image for category from img folder
+	function grabImage(catName, makeSubList) {
+		var imgLi = document.createElement('li');
+		makeSubList.appendChild(imgLi);
+		var imgTag = document.createElement('img');
+		var imgSrc = imgTag.setAttribute("src", "img/" + catName + ".png");
+		imgLi.appendChild(imgTag);
+	}
+
+	// make item links
+	// create edit and delete links for each stored item when displayed
+	function createItemLinks(key, linksLi) {
+		// add edit single item link
+		var editLink = document.createElement('a');
+		editLink.href = "#";
+		editLink.key = key;
+		var editText = "Edit Record /";
+		editLink.addEventListener("click", editEntry);
+		editLink.innerHTML = editText;
+		linksLi.appendChild(editLink);
+		
+		// add line break
+		//var breakTag = document.createElement('br');
+		//linksLi.appendChild(breakTag);
+		
+		// add delete single item links
+		var deleteLink = document.createElement('a');
+		deleteLink.href = "#";
+		deleteLink.key = key;
+		var deleteText = "/ Delete Record";
+		deleteLink.addEventListener("click", deleteItem);
+		deleteLink.innerHTML = deleteText;
+		linksLi.appendChild(deleteLink);
+	}
+
+	function editEntry() {
+		// grab data from localStorage
+		var value = localStorage.getItem(this.key);
+		var item = JSON.parse(value);
+		
+		// show form
+		toggleControls("off");
+		
+		// populate form fields with current stored data
+		go('date').value = item.date[1];
+		go('sugarLevel').value = item.sugar[1];
+		go('pillName').value = item.pillName[1];
+		go('quantity').value = item.quantity[1];
+		if(item.required[1] == "Yes") {
+			go('req').setAttribute("checked", "checked");
+		}
+		go('notes').value = item.notes[1];
+		
+		// remove initial eventListener from save button
+		saveBtn.removeEventListener("click", storeData);
+		
+		// change submit button value to 'edit button'
+		go('submit').value = "Update Record";
+		var editSubmit = go('submit');
+		editSubmit.addEventListener("click", storeData);
+		editSubmit.key = this.key;
+	}
+	
+// populate localStorage with dummyData from json file
 var autofillData = function (){
-	 
+	// store json data into localStorage
+	for(var n in json) {
+		var id = Math.floor(Math.random()*1000001);
+		localStorage.setItem(id, JSON.stringify(json[n]));
+	}
 };
 
 var getData = function(){
 		toggleControls("on");
 		if(localStorage.length === 0) {
 			alert("There is no data to clear, entering dummyData.");
-			enterDummyData();
+			autofillData();
 		}
 		
 		// write data from localStorage to browser
@@ -86,7 +162,7 @@ var getData = function(){
 		}
 };
 
-var storeData = function(data){
+var storeData = function(key){
 		// if no key, create new key
 		if(!key) {
 			// get a random number for localStorage key
@@ -150,4 +226,4 @@ var clearLocal = function(){
 
 	// save data
 	var saveBtn = go('submit');
-	saveBtn.addEventListener("click", validateFields);
+	saveBtn.addEventListener("click", storeData);
