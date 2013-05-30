82248d38463a12451a45518b56122c20c069635c
Kyler Schroeder <kylerschroeder@fullsail.edu>
2013-05-30 16:45:09 -0600

    Display tweak
    Little closer...

--- week4/Gold/js/main.js
+++ week4/Gold/js/main.js
@@ -149,7 +149,7 @@ var getData = function() {
 		// write data from localStorage to browser
 		var makeDiv = document.createElement('div');
 		makeDiv.setAttribute("id", "items");
-		makeDiv.setAttribute("page", "dialog")
+		makeDiv.setAttribute("data-role", "page")
 		var makeList = document.createElement('ul');
 		makeDiv.appendChild(makeList);
 		document.body.appendChild(makeDiv);
